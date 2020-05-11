b026e14ff4132b0cbe5e77826939be2373b105a3
faulknerrainford <kajos.falcon@googlemail.com>
2020-05-11 11:29:45 +0100

    Complete version 0 setup for ALife2020 paper network

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Penny Faulkner Rainford'
 author = 'Penny Faulkner Rainford'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
